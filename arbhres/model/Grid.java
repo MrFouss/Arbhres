26794d0756b0b2c7b9161dba70a16b71a47707c9
MrFouss <maxime.brodat@fouss.fr>
2015-06-03 15:14:34 +0200

    Javadoc added

--- arbhres/model/Grid.java
+++ arbhres/model/Grid.java
@@ -2,67 +2,71 @@ package arbhres.model;
 
 import java.util.Random;
 
+/**
+ * @author	Pierre Brunet <pierre.brunet@krophil.fr>
+ * @version	1.0
+ * @since	05/21/2015
+ */
 public class Grid {
-    private Tile[] tiles;
-    private Queue queue;
-    private Tile inventory;
+	private Tile[] tiles;
+	private Queue queue;
+	private Tile inventory;
 
-    public Grid() {
-	this.tiles = new Tile[16];
-    }
-
-    public Grid(Tile[] tiles) {
-	this.tiles = tiles;
-    }
-
-    public Grid(Grid grid) {
-	this.tiles = grid.tiles;
-	this.queue = grid.queue;
-    }
-
-    public void addFirst(Tile tile) {
-	int i = 0;
-	Random rnd = new Random();
-	do {
-	    add(rnd.nextInt(3) + 1, tile);
-	} while (i < 9);
-    }
-
-    public void add(int coord, Tile tile) {
-	this.tiles[coord] = tile;
-    }
-
-    private int randomTile() {
-	Random rnd = new Random();
-	return rnd.nextInt(2) + 1;
-
-    }
-
-    private int selectAnySide() {
-	Random rnd = new Random();
-	return 3 + (rnd.nextInt(3)) * 4;	
-    }
-
-    private void left(Tile tile, int side) {
-	Tile newTile = new Tile(queue.getQueue().poll());
-	add(selectAnySide(), newTile);
-    }
-    
-    public void rotate(int side){
-	Tile[] newTiles = new Tile[16];
-	int angle;
-	switch(side){
-	case 1:
-	    angle = 90;
-	    break;
-	case 2:
-	    angle = 180;
-	    break;
-	case 3:
-	    angle = 270;
-	    break;
+	public Grid() {
+		this.tiles = new Tile[16];
 	}
-	
-    }
-    
+
+	public Grid(Tile[] tiles) {
+		this.tiles = tiles;
+	}
+
+	public Grid(Grid grid) {
+		this.tiles = grid.tiles;
+		this.queue = grid.queue;
+	}
+
+	public void addFirst(Tile tile) {
+		int i = 0;
+		Random rnd = new Random();
+		do {
+			add(rnd.nextInt(3) + 1, tile);
+		} while (i < 9);
+	}
+
+	public void add(int coord, Tile tile) {
+		this.tiles[coord] = tile;
+	}
+
+	private int randomTile() {
+		Random rnd = new Random();
+		return rnd.nextInt(2) + 1;
+	}
+
+	private int selectAnySide() {
+		Random rnd = new Random();
+		return 3 + (rnd.nextInt(3)) * 4;
+	}
+
+	private void left(Tile tile, int side) {
+		Tile newTile = new Tile(queue.getQueue().poll());
+		add(selectAnySide(), newTile);
+	}
+
+	public void rotate(int side) {
+		Tile[] newTiles = new Tile[16];
+		int angle;
+		switch (side) {
+		case 1:
+			angle = 90;
+			break;
+		case 2:
+			angle = 180;
+			break;
+		case 3:
+			angle = 270;
+			break;
+		}
+
+	}
+
 }
--- arbhres/model/Tile.java
+++ arbhres/model/Tile.java
@@ -1,25 +1,30 @@
 package arbhres.model;
 
+/**
+ * @author	Pierre Brunet <pierre.brunet@krophil.fr>
+ * @version	1.0
+ * @since	05/21/2015
+ */
 public class Tile {
-    int data;
+	int data;
 
-    public Tile() {
-	this.data = 0;
-    }
+	public Tile() {
+		this.data = 0;
+	}
 
-    public Tile(int data) {
-	this.data = data;
-    }
+	public Tile(int data) {
+		this.data = data;
+	}
 
-    public Tile(Tile tile) {
-	this.data = tile.data;
-    }
+	public Tile(Tile tile) {
+		this.data = tile.data;
+	}
 
-    public void setData(int data) {
-	this.data = data;
-    }
+	public void setData(int data) {
+		this.data = data;
+	}
 
-    public int getData() {
-	return this.data;
-    }
+	public int getData() {
+		return this.data;
+	}
 }
